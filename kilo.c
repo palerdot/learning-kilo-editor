f9361c769b5a07ba70e48acc8549ddb77e87e913
palerdot <palerdot@gmail.com>
2017-04-07 16:31:39 +0530

    [chapter 2][FINISH]added different flags input, output and control

--- kilo.c
+++ kilo.c
@@ -1,37 +1,74 @@
+/** includes **/
 #include <ctype.h>
+#include <errno.h>
 #include <unistd.h>
 #include <termios.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/** data **/
 // struct for storing current terminal's original attributes
 struct termios orig_terminal_attrs;
 
+/** error handling **/
+void die(const char *s) {
+    perror(s);
+    exit(1);
+}
+/** terminal operations **/
 // disable raw mode
 void disableRawMode () {
     // restore the terminal with its original terminal attributes
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_terminal_attrs);
+    // tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_terminal_attrs);
+    if( tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_terminal_attrs) == -1 ) die("tcsetattr");
 }
 
 // enable raw mode
 void enableRawMode () {
     // make a copy of the current terminal attributes
-    tcgetattr(STDIN_FILENO, &orig_terminal_attrs);
+    // tcgetattr(STDIN_FILENO, &orig_terminal_attrs);
+    if (tcgetattr(STDIN_FILENO, &orig_terminal_attrs) == -1 ) die("tcgetattr");
     // at the exit of the program disable raw mode
     atexit(disableRawMode);
     // copy the original terminal attributes to our raw
     struct termios raw = orig_terminal_attrs;
 
-    tcgetattr(STDIN_FILENO, &raw);
+    // tcgetattr(STDIN_FILENO, &raw);
+    if (tcgetattr(STDIN_FILENO, &raw) == -1 ) die("tcgetattr");
 
+    // -----------------------------------
+    // input flags:
     // we are doing bitwise AND operator on the ECHO which is a BITWISE value
-    // also disable the canonical mode with ICANON
-    raw.c_lflag &= !(ECHO | ICANON);
+    // disabling software control flow operations
+    // ICRNL => carriage return and new line flag
+    // IXON => ctrl-s and ctrl-q
+    raw.c_iflag &= ~(BRKINT | INPCK | ISTRIP | ICRNL | IXON);
+    // -----------------------------------
+    // disabling OPOST flag for carraige return new line conversion by terminal when outputting
+    // -----------------------------------
+    // output flags:
+    raw.c_oflag &= ~(OPOST);
+    // -----------------------------------
+    // ICANON => also disable the canonical mode with ICANON
+    // ISIG => also disabling SIGINT & SIGTSTP for ctrl-c and ctrl-z
+    // IEXTEN => for disabling ctrl-v
+    // -----------------------------------
+    // control flags:
+    raw.c_cflag |= (CS8);
+    // -----------------------------------
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    // input read timeout values
+    raw.c_cc[VMIN] = 0;
+    // time in milliseconds
+    raw.c_cc[VTIME] = 1;
+    // tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if( tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1 ) die("tcsetattr");
 }
 
 
+/** init **/
 int main () {
 
     enableRawMode();
@@ -41,13 +78,21 @@ int main () {
     // it does so till it scans for Ctrl + D exit character
     // both 'read' and 'STDIN_FILENO' is defined in unistd.h
     // also exit when we type the letter 'q'
-    while( read(STDIN_FILENO, &c, 1) == 1 && c != 'q' ) {
+    while (1) {
+        c = '\0';
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
         if (iscntrl(c)) {
             // printing non printable characters
-            printf("%d \n", c);
+            //carriage return: \r is used to return to the start of the line
+            //new line: \n is used to return to a new line
+            printf("%d \r\n", c);
         } else {
             // printable characters
-            printf("%d ('%c') \n", c, c);
+            printf("%d ('%c') \r\n", c, c);
+        }
+        // break the input queue if we encounter 'q'
+        if (c == 'q') {
+            break;
         }
     }
 
