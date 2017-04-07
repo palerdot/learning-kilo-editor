f33afb85fd6cba345c100c71e91c0b165ea86502
palerdot <palerdot@gmail.com>
2017-04-07 15:07:47 +0530

    [INITIAL COMMIT] converting terminal to raw mode and accepting characters

---
+++ kilo.c
@@ -0,0 +1,45 @@
+#include <unistd.h>
+#include <termios.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// struct for storing current terminal's original attributes
+struct termios orig_terminal_attrs;
+
+// disable raw mode
+void disableRawMode () {
+    // restore the terminal with its original terminal attributes
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_terminal_attrs);
+}
+
+// enable raw mode
+void enableRawMode () {
+    // make a copy of the current terminal attributes
+    tcgetattr(STDIN_FILENO, &orig_terminal_attrs);
+    // at the exit of the program disable raw mode
+    atexit(disableRawMode);
+    // copy the original terminal attributes to our raw
+    struct termios raw = orig_terminal_attrs;
+
+    tcgetattr(STDIN_FILENO, &raw);
+
+    // we are doing bitwise AND operator on the ECHO which is a BITWISE value
+    raw.c_lflag &= !(ECHO);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+
+int main () {
+
+    enableRawMode();
+
+    char c; // gets the current pressed char from the terminal
+    // following line scans for the input character
+    // it does so till it scans for Ctrl + D exit character
+    // both 'read' and 'STDIN_FILENO' is defined in unistd.h
+    // also exit when we type the letter 'q'
+    while( read(STDIN_FILENO, &c, 1) == 1 && c != 'q' );
+
+    return 0;
+}
